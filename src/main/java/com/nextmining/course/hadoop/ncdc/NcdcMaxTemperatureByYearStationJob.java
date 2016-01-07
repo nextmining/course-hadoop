f359922f48d8b8776f84719b7aa42e1caee2c00d
louie <louiezzang@gmail.com>
2016-01-07 20:36:29 +0900

    국립기후자료(NCDC)데이터 분석 실습을 위한 소스 템플릿 커

---
+++ src/main/java/com/nextmining/course/hadoop/ncdc/NcdcMaxTemperatureByYearStationJob.java
@@ -0,0 +1,18 @@
+package com.nextmining.course.hadoop.ncdc;
+
+import com.nextmining.hadoop.mapreduce.AbstractJob;
+
+/**
+ * Max temperature by year and station job.
+ *
+ */
+public class NcdcMaxTemperatureByYearStationJob extends AbstractJob {
+
+	@Override
+	public int run(String[] args) throws Exception {
+		
+		
+		return 0;
+	}
+
+}
